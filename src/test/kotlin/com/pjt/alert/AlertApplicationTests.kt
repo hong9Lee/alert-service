1fef0ba5372ed3009fecc6bd90ec8ca45aa3ceb8
hong9Lee <hongtemp9mac@gmail.com>
2025-07-23 18:11:02 +0900

    init

---
+++ src/test/kotlin/com/pjt/alert/AlertApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pjt.alert
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlertApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
